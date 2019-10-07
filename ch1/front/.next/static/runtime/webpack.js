4dfca5fa9f185958ecf5369ddc417403f0e8199a
김혜지니 <gpwls514@gmail.com>
2019-10-07 17:50:52 +0900

    signup update

--- ch1/front/.next/static/runtime/webpack.js
+++ ch1/front/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "16ef6b6bbc3e7c4ac50a";
+/******/ 	var hotCurrentHash = "103e3be7114100815956";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- ch1/front/pages/profile.js
+++ ch1/front/pages/profile.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import Head from 'next/head';
-import AppLayout from '../components/Applayout';
+import AppLayout from '../components/AppLayout';
 
 const Profile = () => {
     return (
         <>
-            <Head>
-                <title>NodeBird</title>
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.5/antd.css" />
-                <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/3.23.5/antd.js" />
-            </Head>
-            <AppLayout>
-                <div>Profile!</div>
-            </AppLayout>        
+            <div>Profile!</div>
         </>
     );
 }
