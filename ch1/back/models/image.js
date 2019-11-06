82fe87f3577fc456af26a6f8b3e299a24bec3a80
김혜지니 <gpwls514@gmail.com>
2019-11-06 17:09:00 +0900

    back upload

---
+++ ch1/back/models/image.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+    const Image = sequelize.define('Image', {
+        src: { // S3 저장
+            type: DataTypes.STRING(200), 
+            allowNull: false,  // 필수 (true면 선택)
+        },
+    }, {
+        charset: 'utf8',
+        collate: 'utf8_general_ci', // 한글이 저장됨
+    });
+
+    Image.associate = (db) => {
+        db.Image.belongsTo(db.Post);
+    };
+
+    return Image;
+};
